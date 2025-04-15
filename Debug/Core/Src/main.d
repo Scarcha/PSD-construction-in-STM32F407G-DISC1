9260dc8aff76c87778804e84adfa40b5c5a96962
Scarcha <76191015+Scarcha@users.noreply.github.com>
2025-04-15 18:56:49 -0400

    V1.0 Funcionalidad Básica FFT

---
+++ Debug/Core/Src/main.d
@@ -0,0 +1,138 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Core/Inc/stm32f4xx_hal_conf.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h ../Core/Inc/i2c.h \
+ ../Core/Inc/main.h ../Core/Inc/i2s.h ../Core/Inc/spi.h \
+ ../USB_HOST/App/usb_host.h ../Core/Inc/gpio.h \
+ ../Drivers/CMSIS_DSP/Include/arm_math.h \
+ ../Drivers/CMSIS_DSP/Include/arm_math_types.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS_DSP/Include/arm_math_memory.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/none.h \
+ ../Drivers/CMSIS_DSP/Include/arm_math_types.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/utils.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/basic_math_functions.h \
+ ../Drivers/CMSIS_DSP/Include/arm_math_memory.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/none.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/utils.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/interpolation_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/bayes_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/statistics_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/basic_math_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/fast_math_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/matrix_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/complex_math_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/statistics_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/controller_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/support_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/distance_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/matrix_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/svm_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/svm_defines.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/fast_math_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/transform_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/complex_math_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/filtering_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/support_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/quaternion_math_functions.h \
+ ../Drivers/CMSIS_DSP/Include/dsp/window_functions.h
+../Core/Inc/main.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Core/Inc/stm32f4xx_hal_conf.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+../Core/Inc/i2c.h:
+../Core/Inc/main.h:
+../Core/Inc/i2s.h:
+../Core/Inc/spi.h:
+../USB_HOST/App/usb_host.h:
+../Core/Inc/gpio.h:
+../Drivers/CMSIS_DSP/Include/arm_math.h:
+../Drivers/CMSIS_DSP/Include/arm_math_types.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS_DSP/Include/arm_math_memory.h:
+../Drivers/CMSIS_DSP/Include/dsp/none.h:
+../Drivers/CMSIS_DSP/Include/arm_math_types.h:
+../Drivers/CMSIS_DSP/Include/dsp/utils.h:
+../Drivers/CMSIS_DSP/Include/dsp/basic_math_functions.h:
+../Drivers/CMSIS_DSP/Include/arm_math_memory.h:
+../Drivers/CMSIS_DSP/Include/dsp/none.h:
+../Drivers/CMSIS_DSP/Include/dsp/utils.h:
+../Drivers/CMSIS_DSP/Include/dsp/interpolation_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/bayes_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/statistics_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/basic_math_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/fast_math_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/matrix_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/complex_math_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/statistics_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/controller_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/support_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/distance_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/matrix_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/svm_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/svm_defines.h:
+../Drivers/CMSIS_DSP/Include/dsp/fast_math_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/transform_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/complex_math_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/filtering_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/support_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/quaternion_math_functions.h:
+../Drivers/CMSIS_DSP/Include/dsp/window_functions.h:
